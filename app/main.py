369b89876a64ce24447ff1549af1c3383e2cbdab
MonkeyMacho7 <ethanlarson@outlook.com>
2025-04-07 18:35:54 -0600

    changes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.services.stats_repository import MessiRepository
 from app.schemas import MessiStat
 from typing import List
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 repo = MessiRepository()
 
 @app.get("/stats", response_model=List[MessiStat])
