5a3dbd4bd48b55baefb5edc6487aeaaca0517cd1
MonkeyMacho7 <ethanlarson@outlook.com>
2025-04-15 20:25:29 -0600

    Update stats_routes.py

--- app/services/stats_routes.py
+++ app/services/stats_routes.py
@@ -9,4 +9,5 @@ router = APIRouter()
 @router.get("/stats", response_model=list[MessiStat])
 def get_all_stats(db: Session = Depends(get_db)):
     stats = db.query(MessiStatDB).all()
+    print(" Fetching from Postgres DB!")
     return stats
